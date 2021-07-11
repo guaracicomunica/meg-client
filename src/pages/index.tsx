85a389e5b409cba3d0ac7bc68f7154143c6268e2
Fabiana Pereira <fabiana.duarte@hotmail.com.br>
2021-07-11 19:25:57 -0300

    feat: add keywords section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,13 +18,32 @@ export default function Home() {
 
       <main>
         <section className={`${styles["home-banner"]}`}>
-          <div className="col-8 col-md-4 mt-md-5 flex-column">
+          <div className="col-8 col-md-4 flex-column">
             <img src="./images/meg-logo.svg" alt="Logo do MEG" className={styles["logo-home"]} />
             <h1 className={`${styles["title-home"]} mt-4`}>Seja bem-vindo ao Mundo Encantado da Geografia!</h1>
           </div>
 
-          <div className="col-6 col-md-5 my-5 mb-md-0">
+          <div className="col-6 col-lg-5 my-5 my-md-0">
             <img src="./images/illustration-home.svg" className="img-fluid" />
+          </div>
+        </section>
+
+        <section className="section d-flex flex-column align-items-center">
+          <div className="col-11 col-lg-5 justify-content-center">
+            <h1 className="title-primary text-center">Conheça o Mundo Encantado da Geografia (MEG)</h1>
+            <hr className="border-section" />
+          </div>
+          
+          <div className="col-11 col-lg-7">
+            <p className="text-section text-center">Donec accumsan, dolor ac malesuada rhoncus, leo arcu pellentesque dolor, nec tristique diam neque vitae sem. Nulla a lectus sollicitudin, volutpat lacus id, eleifend ipsum. Donec accumsan, dolor ac malesuada rhoncus, leo arcu pellentesque dolor, nec tristique diam neque vitae sem.</p>
+          </div>
+          
+          <div className="col-11 col-md-7 d-flex flex-wrap justify-content-center">
+            <div className={styles.keyword}>gamificação</div>
+            <div className={styles.keyword}>geografia</div>
+            <div className={styles.keyword}>arte</div>
+            <div className={styles.keyword}>diversidade</div>
+            <div className={styles.keyword}>tecnologia</div>
           </div>
         </section>
       </main>

7c6e20e396677dee057fd8e02b105a3d20cf7dfa
Fabiana Pereira <fabiana.duarte@hotmail.com.br>
2021-07-11 18:29:01 -0300

    feat: add banner home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,6 +17,16 @@ export default function Home() {
       </header>
 
       <main>
+        <section className={`${styles["home-banner"]}`}>
+          <div className="col-8 col-md-4 mt-md-5 flex-column">
+            <img src="./images/meg-logo.svg" alt="Logo do MEG" className={styles["logo-home"]} />
+            <h1 className={`${styles["title-home"]} mt-4`}>Seja bem-vindo ao Mundo Encantado da Geografia!</h1>
+          </div>
+
+          <div className="col-6 col-md-5 my-5 mb-md-0">
+            <img src="./images/illustration-home.svg" className="img-fluid" />
+          </div>
+        </section>
       </main>
 
       <Footer />

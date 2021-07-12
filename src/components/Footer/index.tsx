467a1ac2689f8f33b64c58ef5ab3a250b70efddb
Fabiana Pereira <fabiana.duarte@hotmail.com.br>
2021-07-12 18:20:56 -0300

    feat: add footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,7 +1,16 @@
 import styles from "./styles.module.css";
 
 export function Footer() {
-    return (
-        <></>
-    )
+  return (
+    <footer>
+      <hr className={styles["border-footer"]} />
+
+      <div className="row flex-wrap p-4 justify-content-center">
+        <a href="/" className={`${styles["link-footer"]} mr-4`}>Início</a>
+        <a href="#" className={`${styles["link-footer"]} mr-4`}>Turmas</a>
+        <a href="#" className={`${styles["link-footer"]} mr-4`}>Atividades</a>
+        <a href="#" className={styles["link-footer"]}>Acessar</a>
+      </div>
+    </footer>
+  );
 }

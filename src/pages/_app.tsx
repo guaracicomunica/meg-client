1c23f1aa4fdf10ea69e959e072960bc7bf8c5e4f
Fabiana Pereira <fabianaduarte980@gmail.com>
2021-12-21 17:48:07 -0300

    style: add form stylesheet

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,6 +7,7 @@ import { Footer } from '../components/Footer';
 import 'bootstrap/dist/css/bootstrap.css';
 import '../styles/global.css';
 import '../styles/buttons.css';
+import '../styles/form.css';
 
 function MyApp({ Component, pageProps }) {
   return (

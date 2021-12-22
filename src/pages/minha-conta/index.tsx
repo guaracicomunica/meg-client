2f591c181454df3df684127ce49392a50108f4a2
Fabiana Pereira <fabianaduarte980@gmail.com>
2021-12-22 16:38:05 -0300

    feat: create other pages (turmas, atividades, minha conta)

--- src/pages/minha-conta/index.tsx
+++ src/pages/minha-conta/index.tsx
@@ -1,15 +1,15 @@
 import { GetServerSideProps } from "next";
 import { useContext } from "react"
 import { parseCookies } from 'nookies'
-import { AuthContext } from "../contexts/AuthContext"
-import { getAPIClient } from "../services/apiClient";
+import { AuthContext } from "../../contexts/AuthContext"
+import { getAPIClient } from "../../services/apiClient";
 
 export default function Dashboard() {
   const { user, logoff } = useContext(AuthContext);
 
   return (
     <>
-      <h1>usuário autenticado</h1>
+      <h1>minha conta</h1>
       <p>user: {user?.name} - {user?.email}</p>
       <button onClick={logoff}>sair da conta</button>
     </>

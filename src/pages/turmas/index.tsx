0852db68a959b3c41f8a23d0562e8868246793d5
Fabiana Pereira <fabianaduarte980@gmail.com>
2021-12-29 01:01:52 -0300

    feat: add infinite scroll in classes page

--- src/pages/turmas/index.tsx
+++ src/pages/turmas/index.tsx
@@ -1,23 +1,29 @@
 import { GetServerSideProps } from 'next';
 import Head from 'next/head';
 import { useContext, useState } from 'react';
-import { OverlayTrigger, Tooltip } from 'react-bootstrap';
+import { OverlayTrigger, Spinner, Tooltip } from 'react-bootstrap';
 import { parseCookies } from 'nookies';
 import { ToastContainer } from 'react-toastify';
+import InfiniteScroll from 'react-infinite-scroll-component';
 
+import CardClass from '../../components/CardClass';
 import ModalCreateNewClass from '../../components/ModalCreateNewClass';
 import ModalAddClass from '../../components/ModalAddClass';
-import CardClass from '../../components/CardClass';
 import { AuthContext } from '../../contexts/AuthContext';
+import { ClassStatus } from '../../enums/enumClassStatus';
+import { api } from '../../services/api';
 import { getAPIClient } from '../../services/apiClient';
 import { RoleUser } from '../../enums/enumRoleUser';
-import { ClassStatus } from '../../enums/enumClassStatus';
 import { ClassCard } from '../../types/Class';
 
 import styles from './styles.module.css';
 
 type ClassPageType = {
   classes: ClassCard[];
+  queryProps: {
+    currentPage: number;
+    totalPages: number;
+  }
 }
 
 export default function Turmas(props: ClassPageType) {
@@ -25,6 +31,23 @@ export default function Turmas(props: ClassPageType) {
 
   const [showModalTeacher, setShowModalTeacher] = useState(false);
   const [showModalStudent, setShowModalStudent] = useState(false);
+  const [classes, setClasses] = useState(props.classes);
+  const [hasMore, setHasMore] = useState(true);
+
+  async function getMorePost() {
+    const response = await api.get('classes', {
+      params: {
+        page: props.queryProps.currentPage + 1,
+        per_page: 10
+      }
+    });
+
+    if (response.data.current_page === props.queryProps.totalPages) {
+      setHasMore(false);
+    }
+
+    setClasses([...classes, ...response.data.data]);
+  }
 
   function openModal(role: number) {
     if (role === RoleUser.teacher) {
@@ -41,68 +64,76 @@ export default function Turmas(props: ClassPageType) {
         <title>Turmas</title>
       </Head>
 
-      <main className={styles["classes-list"]}>
-        {/* show active classes first */}
-        {props.classes.length > 0 && (
-          props.classes.map(classroom => {
-            if (classroom.status === ClassStatus.active) {
-              return (
-                <CardClass
-                  key={classroom.id}
-                  id={classroom.id}
-                  name={classroom.name}
-                  nickname={classroom.nickname}
-                  teacher={user?.name}
-                  code={classroom.code}
-                  banner={classroom.banner}
-                  roleUser={user?.role}
-                  status={classroom.status}
-                  partners={classroom.partners}
-                  levels={classroom.levels}
-                  skills={classroom.skills}
-                />
-              );
-            }
-          })
-        )}
+      <main>
+        <InfiniteScroll
+          className={styles["classes-list"]}
+          dataLength={classes.length}
+          next={getMorePost}
+          hasMore={hasMore}
+          loader={<div className={styles["loading-container"]}><Spinner animation="border" /></div>}
+        >
+          {/* show active classes first */}
+          {classes.length > 0 && (
+            classes.map(classroom => {
+              if (classroom.status === ClassStatus.active) {
+                return (
+                  <CardClass
+                    key={classroom.id}
+                    id={classroom.id}
+                    name={classroom.name}
+                    nickname={classroom.nickname}
+                    teacher={user?.name}
+                    code={classroom.code}
+                    banner={classroom.banner}
+                    roleUser={user?.role}
+                    status={classroom.status}
+                    partners={classroom.partners}
+                    levels={classroom.levels}
+                    skills={classroom.skills}
+                  />
+                );
+              }
+            })
+          )}
 
-        {/* show inactive classes */}
-        {props.classes.length > 0 && (
-          props.classes.map(classroom => {
-            if (classroom.status === ClassStatus.inactive) {
-              return (
-                <CardClass
-                  key={classroom.id}
-                  id={classroom.id}
-                  name={classroom.name}
-                  nickname={classroom.nickname}
-                  teacher={user?.name}
-                  code={classroom.code}
-                  banner={classroom.banner}
-                  roleUser={user?.role}
-                  status={classroom.status}
-                  partners={classroom.partners}
-                  levels={classroom.levels}
-                  skills={classroom.skills}
-                />
-              );
-            }
-          })
-        )}
+          {/* show inactive classes */}
+          {classes.length > 0 && (
+            classes.map(classroom => {
+              if (classroom.status === ClassStatus.inactive) {
+                return (
+                  <CardClass
+                    key={classroom.id}
+                    id={classroom.id}
+                    name={classroom.name}
+                    nickname={classroom.nickname}
+                    teacher={user?.name}
+                    code={classroom.code}
+                    banner={classroom.banner}
+                    roleUser={user?.role}
+                    status={classroom.status}
+                    partners={classroom.partners}
+                    levels={classroom.levels}
+                    skills={classroom.skills}
+                  />
+                );
+              }
+            })
+          )}
 
-        <div className={styles["add-class"]} onClick={() => openModal(user?.role)}>
-          <OverlayTrigger
-            key="tooltip-add-class"
-            placement="bottom"
-            overlay={
-              <Tooltip id="tooltip">
-                {user?.role === RoleUser.teacher ? "Criar nova turma" : "Adicionar nova turma"}
-              </Tooltip>
-            }
-          >
-            <img src="./icons/plus.svg" alt="Adicionar turma" />
-          </OverlayTrigger>
-        </div>
+          <div className={styles["add-class"]} onClick={() => openModal(user?.role)}>
+            <OverlayTrigger
+              key="tooltip-add-class"
+              placement="bottom"
+              overlay={
+                <Tooltip id="tooltip">
+                  {user?.role === RoleUser.teacher ? "Criar nova turma" : "Adicionar nova turma"}
+                </Tooltip>
+              }
+            >
+              <img src="./icons/plus.svg" alt="Adicionar turma" />
+            </OverlayTrigger>
+          </div>
+        </InfiniteScroll>
 
         <ModalCreateNewClass
           type="create"
@@ -135,12 +166,21 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
   else {
     apiClient.defaults.headers['Authorization'] = `Bearer ${token}`;
 
-    const response = await apiClient.get('classes');
+    const response = await apiClient.get('classes', {
+      params: {
+        per_page: 10
+      }
+    });
     const classes = response.data.data;
+    const queryProps = {
+      currentPage: response.data.current_page,
+      totalPages: response.data.last_page,
+    }
 
     return {
       props: {
-        classes
+        classes,
+        queryProps
       }
     }
   }  

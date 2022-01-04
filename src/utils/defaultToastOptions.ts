5148dcb27875f2ae08c18a24d717ea602bb1e927
João Paulo M <joaopaulopmedeiros@gmail.com>
2022-01-04 14:47:15 -0300

    style: increate time of toast autoclose

--- src/utils/defaultToastOptions.ts
+++ src/utils/defaultToastOptions.ts
@@ -3,7 +3,7 @@ import { ToastOptions } from "react-toastify";
 //configurações, por padrão, adotadas no projeto
 export const options: ToastOptions = {
   position: "top-right",
-  autoClose: 5000,
+  autoClose: 10000,
   hideProgressBar: false,
   closeOnClick: true,
   pauseOnHover: true,

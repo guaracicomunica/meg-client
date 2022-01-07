aa07260538a841b85ae67d823bfb3ce87c6849e3
Fabiana Pereira <fabianaduarte980@gmail.com>
2022-01-07 19:07:42 -0300

    style: fix margin in post activity component

--- src/components/PostActivity/index.tsx
+++ src/components/PostActivity/index.tsx
@@ -5,7 +5,7 @@ import styles from './styles.module.css';
 
 export default function PostActivity(props: PostActivityType) {
   return (
-    <div className={`${styles["post-activity"]} mt-3 py-4 px-5`}>
+    <div className={`${styles["post-activity"]} mb-3 py-4 px-5`}>
       <img src="/icons/activity-post.svg" alt="Atividade" />
       <h5>Prof. {props.teacher} postou uma nova atividade: <Link href="#">{props.title}</Link></h5>
     </div>

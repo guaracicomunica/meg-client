e95085b6be5281d243c06e33a45ec60d1a0fc430
Fabiana Pereira <fabianaduarte980@gmail.com>
2022-01-10 20:40:09 -0300

    feat: student can enroll in a class

--- src/components/ModalAddClass/index.tsx
+++ src/components/ModalAddClass/index.tsx
@@ -1,11 +1,82 @@
+import { useRouter } from 'next/router';
+import { parseCookies } from 'nookies';
 import { Modal } from 'react-bootstrap';
+import { useForm } from 'react-hook-form';
+import { toast } from 'react-toastify';
+
+import { api } from '../../services/api';
+import { options } from '../../utils/defaultToastOptions';
 
 type ModalAddClassType = {
   show: boolean;
   onHide: () => void;
 }
 
+type EnrollmentForm = {
+  code: string;
+}
+
 export default function ModalAddClass(props: ModalAddClassType) {
+  const { 'meg.token': token } = parseCookies();
+  const router = useRouter();
+
+  const { register, handleSubmit, reset } = useForm({defaultValues: {
+    code: "",
+  }});
+  const onSubmit = async (data: EnrollmentForm) => handleCreateClass(data);
+
+  async function handleCreateClass(data: EnrollmentForm) {
+    try {
+      await api.post('classes/enrollment', data, {
+        headers: {
+          'Authorization': `Bearer ${token}`
+        }
+      })
+      .then(function (success) {
+        reset({
+          code: ""
+        });
+        toast.success("Cadastro na turma realizado com sucesso!", options);
+        router.push("/turmas");
+        props.onHide();
+      });
+    }
+    catch(error) {
+      const string = "Ops! Algo não saiu como o esperado. Tente novamente ou entre em contato com o suporte.";
+
+      if (!error.response) {
+        // network error
+        return toast.error(string, options);
+      }
+      switch (error.response.status) {
+        case 401:
+          return {
+            redirect: {
+              destination: '/sessao-expirada',
+              permanent: false,
+            }
+          }
+        
+        case 400:
+          toast.warning(error.response?.data.error.trim() ? error.response?.data.error.trim() : string, options);
+
+        case 422:
+          let errors = error.response?.data.errors;
+          Object.keys(errors).forEach((item) => {
+            toast.warning(errors[item][0], options);
+          });
+
+        case 500: 
+          toast.error(string, options);
+          break;
+
+        default:
+          toast.error(string, options);
+          break;
+      }
+    }
+  }
+
   return (
     <Modal
       id="modal-add-class-student"
@@ -22,19 +93,20 @@ export default function ModalAddClass(props: ModalAddClassType) {
         </Modal.Title>
       </Modal.Header>
       <Modal.Body className='px-4'>
-        <form>
+        <form autoComplete='off' id="enroll-class" method='post' onSubmit={handleSubmit(onSubmit)}>
           <div className="form-group">
             <input
               type="text"
               className="form-control form-input w-100"
               id="code"
               placeholder="Código da turma"
+              {...register('code')}
             />
           </div>
         </form>
       </Modal.Body>
       <Modal.Footer className='d-flex justify-content-end p-4 border-top-0'>
-        <button className="modal-button">Entrar</button>
+        <button form="enroll-class" type="submit" className="modal-button">Entrar</button>
       </Modal.Footer>
     </Modal>
   );

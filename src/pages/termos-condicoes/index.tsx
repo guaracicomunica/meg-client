e6cd5b3ee7502660fba7d6d63813753d2151f757
Fabiana Pereira <fabianaduarte980@gmail.com>
2022-01-14 15:16:30 -0300

    feat: add page to terms and conditions

---
+++ src/pages/termos-condicoes/index.tsx
@@ -0,0 +1,28 @@
+import Head from 'next/head';
+
+import styles from './styles.module.css';
+
+export default function TermosCondicoes() {
+  return (
+    <>
+      <Head>
+        <title>Termos e condições do MEG</title>
+      </Head>
+
+      <main className={styles["page-layout"]}>
+        <div className='col-10 col-md-9 col-lg-8 col-xl-7'>
+          <h1 className='title-primary mb-4'>Termos e condições do MEG</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Massa sapien faucibus et molestie ac feugiat sed lectus vestibulum. Vestibulum rhoncus est pellentesque elit ullamcorper dignissim cras tincidunt. Tristique et egestas quis ipsum suspendisse ultrices gravida dictum. Porttitor massa id neque aliquam vestibulum morbi. Iaculis at erat pellentesque adipiscing commodo elit at. Lacus laoreet non curabitur gravida arcu. Urna condimentum mattis pellentesque id. Donec enim diam vulputate ut. Nullam vehicula ipsum a arcu cursus. Scelerisque mauris pellentesque pulvinar pellentesque habitant morbi tristique senectus. Viverra vitae congue eu consequat ac felis. Aliquam sem fringilla ut morbi. Donec ac odio tempor orci. Volutpat consequat mauris nunc congue nisi. Nisl suscipit adipiscing bibendum est ultricies. Aliquam id diam maecenas ultricies. Adipiscing elit duis tristique sollicitudin nibh sit amet commodo. Integer malesuada nunc vel risus commodo viverra. Viverra ipsum nunc aliquet bibendum enim.</p>
+
+          <p>Nisi est sit amet facilisis magna etiam. Convallis aenean et tortor at risus. A arcu cursus vitae congue mauris rhoncus. Nisl suscipit adipiscing bibendum est. Fusce id velit ut tortor pretium viverra suspendisse potenti nullam. Urna porttitor rhoncus dolor purus non enim. Faucibus interdum posuere lorem ipsum dolor sit amet consectetur adipiscing. Libero nunc consequat interdum varius sit amet. Massa eget egestas purus viverra accumsan in nisl nisi scelerisque. Vestibulum sed arcu non odio euismod lacinia at quis risus. Cras adipiscing enim eu turpis egestas pretium. Lectus magna fringilla urna porttitor rhoncus dolor. Leo integer malesuada nunc vel risus commodo viverra maecenas. Nunc aliquet bibendum enim facilisis gravida neque convallis. Adipiscing at in tellus integer feugiat. Egestas quis ipsum suspendisse ultrices gravida dictum fusce ut. Vulputate eu scelerisque felis imperdiet proin fermentum. Aliquam sem et tortor consequat id porta nibh venenatis. Imperdiet nulla malesuada pellentesque elit eget gravida cum sociis natoque. Magna sit amet purus gravida.</p>
+
+          <p>Adipiscing bibendum est ultricies integer quis auctor elit. Ac tincidunt vitae semper quis lectus nulla at. Proin nibh nisl condimentum id venenatis a condimentum vitae. Enim lobortis scelerisque fermentum dui faucibus in ornare quam. Lorem ipsum dolor sit amet consectetur adipiscing elit ut. Ornare arcu dui vivamus arcu felis bibendum ut tristique. Eget nullam non nisi est sit amet facilisis magna etiam. Suspendisse sed nisi lacus sed viverra tellus in hac. Scelerisque felis imperdiet proin fermentum leo. Nunc mattis enim ut tellus elementum sagittis vitae. Eu nisl nunc mi ipsum faucibus vitae. Nulla aliquet porttitor lacus luctus accumsan tortor posuere ac ut. Eu turpis egestas pretium aenean. Tristique magna sit amet purus gravida. Ut morbi tincidunt augue interdum velit euismod in. Adipiscing elit ut aliquam purus sit.</p>
+
+          <p>Erat imperdiet sed euismod nisi porta lorem. Pellentesque dignissim enim sit amet venenatis urna. Cursus in hac habitasse platea dictumst quisque sagittis. Consectetur adipiscing elit ut aliquam purus sit amet luctus. Gravida rutrum quisque non tellus orci ac auctor augue mauris. Sit amet cursus sit amet dictum. Enim ut tellus elementum sagittis vitae et leo duis ut. Habitant morbi tristique senectus et netus et. Lacus sed viverra tellus in hac habitasse. Nullam ac tortor vitae purus faucibus ornare. Suspendisse sed nisi lacus sed viverra tellus in. Id donec ultrices tincidunt arcu. Dolor magna eget est lorem ipsum dolor sit amet consectetur.</p>    
+
+          <p>Proin sed libero enim sed faucibus turpis. Eget nullam non nisi est sit amet facilisis magna etiam. Massa tincidunt dui ut ornare lectus sit. Cras sed felis eget velit aliquet sagittis id consectetur. Nunc sed augue lacus viverra vitae congue. Tristique nulla aliquet enim tortor at auctor. Mattis ullamcorper velit sed ullamcorper morbi tincidunt ornare massa eget. Sit amet nisl suscipit adipiscing. Ac tortor vitae purus faucibus ornare suspendisse sed nisi. Sollicitudin aliquam ultrices sagittis orci a scelerisque purus. Porttitor lacus luctus accumsan tortor posuere ac ut consequat semper. Faucibus in ornare quam viverra. Faucibus vitae aliquet nec ullamcorper. Semper feugiat nibh sed pulvinar proin gravida hendrerit lectus a.</p>
+        </div>
+      </main>
+    </>
+  );
+}

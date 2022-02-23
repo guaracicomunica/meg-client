88a0f48bf2aaa69101eeed4a81f902216c08f953
Fabiana Pereira <fabianaduarte980@gmail.com>
2022-02-23 00:15:59 -0300

    styles: add high contrast in session expired page

--- src/pages/sessao-expirada/index.tsx
+++ src/pages/sessao-expirada/index.tsx
@@ -1,21 +1,25 @@
 import Head from "next/head";
 import Link from "next/link";
+import { useContext } from "react";
+
+import { ThemeContext } from "../../contexts/ThemeContext";
 
 import styles from './styles.module.css';
 
 export default function SessaoExpirada() {
+  const { theme } = useContext(ThemeContext);
+
   return (
     <>
       <Head>
         <title>Sessão expirada</title>
       </Head>
 
-      <main className={`${styles.page} section`}>
-        <img src="./images/session-expired.svg" alt="Sessão expirada" />
-        <h1>Ops... Você está desconectado(a)!</h1>
-        <p>Clique <Link href="/login">aqui</Link> para se conectar novamente.</p>
+      <main className="center-content section">
+        <img className={styles["img-page"]} src="./images/session-expired.svg" alt="Sessão expirada" />
+        <h1 className={`title-${theme}-primary`}>Ops... Você está desconectado(a)!</h1>
+        <p className={`text-section-${theme}`}>Clique <Link href="/login">aqui</Link> para se conectar novamente.</p>
       </main>
     </>
-    
   );
 }

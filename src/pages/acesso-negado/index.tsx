185b2516be48e38c18a3886825dbf2d7bd303ade
Fabiana Pereira <fabianaduarte980@gmail.com>
2022-02-23 00:08:41 -0300

    styles: add high contrast to access forbidden page

--- src/pages/acesso-negado/index.tsx
+++ src/pages/acesso-negado/index.tsx
@@ -1,21 +1,25 @@
 import Head from "next/head";
 import Link from "next/link";
+import { useContext } from "react";
+
+import { ThemeContext } from "../../contexts/ThemeContext";
 
 import styles from './styles.module.css';
 
 export default function AcessoNegado() {
+  const { theme } = useContext(ThemeContext);
+
   return (
     <>
       <Head>
         <title>Acesso Negado</title>
       </Head>
 
-      <main className={`${styles.page} section`}>
+      <main className={`${styles.page} center-content section`}>
         <img src="./icons/access-denied.svg" alt="Acesso negado" /> 
-        <h1>Ops... Você não possui permissão para realizar esta ação!</h1>
-        <p>Clique <Link href="/">aqui</Link> para voltar à página inicial.</p>
+        <h1 className={`title-${theme}-primary`}>Ops... Você não possui permissão para realizar esta ação!</h1>
+        <p className={`text-section-${theme}`}>Clique <Link href="/">aqui</Link> para voltar à página inicial.</p>
       </main>
     </>
-    
   );
 }

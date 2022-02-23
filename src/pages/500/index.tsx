5af26a302fbb53bd51bb97c544d177be778db9b9
Fabiana Pereira <fabianaduarte980@gmail.com>
2022-02-23 00:06:10 -0300

    styles: add high contrast to 500 error page

--- src/pages/500/index.tsx
+++ src/pages/500/index.tsx
@@ -1,21 +1,26 @@
 import Head from "next/head";
 import Link from "next/link";
+import { useContext } from "react";
+
+import { ThemeContext } from "../../contexts/ThemeContext";
+import { enumTheme } from "../../enums/enumTheme";
 
 import styles from './styles.module.css';
 
 export default function Custom500() {
+  const { theme } = useContext(ThemeContext);
+
   return (
     <>
       <Head>
         <title>Erro inesperado</title>
       </Head>
 
-      <main className={`${styles.page} section`}>
+      <main className={`${styles.page} center-content section`}>
         <img src="./icons/500.svg" alt="Erro inesperado" /> 
-        <h1>Ops... erro inesperado!</h1>
-        <p>Clique <Link href="/">aqui</Link> para voltar à página inicial.</p>
+        <h1 className={`title-${theme}-primary`}>Ops... erro inesperado!</h1>
+        <p className={`text-section-${theme}`}>Clique <Link href="/">aqui</Link> para voltar à página inicial.</p>
       </main>
     </>
-    
   );
 }

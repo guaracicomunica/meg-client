2b767cc6ee9c1508a067b47a7ea65b5d02a08b6a
Fabiana Pereira <fabianaduarte980@gmail.com>
2022-03-12 23:00:06 -0300

    styles: add high contrast in classroom participants page

--- src/components/CardStudent/index.tsx
+++ src/components/CardStudent/index.tsx
@@ -1,5 +1,7 @@
 import { useContext, useState } from 'react';
 import { AuthContext } from '../../contexts/AuthContext';
+import { ThemeContext } from '../../contexts/ThemeContext';
+import { enumTheme } from '../../enums/enumTheme';
 import { RoleUser } from '../../enums/enumRoleUser';
 import { StudentType } from '../../types/Participant';
 import ModalShowStudent from '../ModalShowStudent';
@@ -13,15 +15,18 @@ type CardStudentProps = {
 export default function CardStudent(props: CardStudentProps) {
   const [showModalSeeStudent, setShowModalSeeStudent] = useState(false);
   const { user } = useContext(AuthContext);
+  const { theme } = useContext(ThemeContext);
+  const isHighContrast = theme === enumTheme.contrast;
+  const isTeacher = user?.role === RoleUser.teacher
 
   const { student } = props;
 
   return (
     <>
-      <div className="card-style p-4">
+      <div className={`card-style p-4 ${styles[`card-student-${theme}`]}`}>
         <div className={styles["info-user"]}>
           <img src={student.avatar ?? "/icons/user.svg"} alt="Imagem do aluno" />
-          <h5>{student.name}</h5>
+          <h5 style={!isHighContrast ? { color: 'var(--gray-title)' } : {}}>{student.name}</h5>
         </div>
         <div className={styles["score-user"]}>
           <div className={styles["points-user"]}>
@@ -33,7 +38,7 @@ export default function CardStudent(props: CardStudentProps) {
             <span>{student.level ? `Nível ${student.level} - ${student.levelName}` : "Nível inicial"}</span>
           </div>
         </div>
-        {user?.role === RoleUser.teacher && (
+        {isTeacher && (
           <div className="d-flex justify-content-center mt-4 w-100">
             <button
               onClick={() => setShowModalSeeStudent(true)}

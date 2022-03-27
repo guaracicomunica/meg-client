61461a894bfe2ad9bc12c30adfd02252591de56a
Fabiana Pereira <fabianaduarte980@gmail.com>
2022-03-26 21:23:24 -0300

    fix: change js script

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,4 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
-import Script from 'next/script';
 
 export default class MyDocument extends Document {
   render() {
@@ -15,21 +14,21 @@ export default class MyDocument extends Document {
           <link href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700;900&display=swap" rel="stylesheet" />
           <link rel="icon" href="/icons/logo.svg" />
 
-          <Script
+          <script
             src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
             integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
             crossOrigin="anonymous"
-          />
-          <Script
+          ></script>
+          <script
             src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
             integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
             crossOrigin="anonymous"
-          />
-          <Script
+          ></script>
+          <script
             src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
             integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
             crossOrigin="anonymous"
-          />
+          ></script>
         </Head>
 
         <body>
